6de8d532775365bac89328e494990802eff3c458
Henrique Pinheiro <henrique@gampoc.com>
2024-02-29 14:19:38 -0300

    Add cache headers

--- routes/api/rates/[currency].ts
+++ routes/api/rates/[currency].ts
@@ -11,6 +11,13 @@ const schema = z.object({
 
 export const handler = async (req: Request, ctx: FreshContext) => {
   const origin = req.headers.get("Origin") || "*";
+  const headers = {
+    "Access-Control-Allow-Origin": origin,
+    "Access-Control-Allow-Methods": "GET",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Cache-Control": "public, max-age=3600",
+    "Surrogate-Control": "public, max-age=3600",
+  };
   let { currency } = ctx.params;
   let start = ctx.url.searchParams.get("start") ?? undefined;
   let end = ctx.url.searchParams.get("end") ?? undefined;
@@ -19,14 +26,14 @@ export const handler = async (req: Request, ctx: FreshContext) => {
   } catch (e) {
     return new Response(e.message, {
       status: 400,
-      headers: { "Access-Control-Allow-Origin": origin },
+      headers,
     });
   }
   const data = await getExchangeRate(currency, start, end);
   return new Response(JSON.stringify(data), {
     headers: {
       "content-type": "application/json",
-      "Access-Control-Allow-Origin": origin,
+      ...headers,
     },
   });
 };
