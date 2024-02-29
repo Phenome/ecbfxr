9ed7f7e10c27ab2ddee59a24694cae0b040fcacd
Henrique Pinheiro <henrique@gampoc.com>
2024-02-29 13:47:32 -0300

    Fix handling lowercase currency codes

--- routes/api/rates/[currency].ts
+++ routes/api/rates/[currency].ts
@@ -4,17 +4,17 @@ import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
 import { getExchangeRate } from "../../../utils/ecbApi.ts";
 
 const schema = z.object({
-  currency: z.string().length(3),
-  start: z.string().regex(/^\d{4}-\d{2}-\d{2}$/).nullish(),
-  end: z.string().regex(/^\d{4}-\d{2}-\d{2}$/).nullish(),
+  currency: z.string().length(3).toUpperCase(),
+  start: z.string().regex(/^\d{4}-\d{2}-\d{2}$/).optional(),
+  end: z.string().regex(/^\d{4}-\d{2}-\d{2}$/).optional(),
 });
 
 export const handler = async (_req: Request, _ctx: FreshContext) => {
-  const { currency } = _ctx.params;
-  const start = _ctx.url.searchParams.get("start") ?? undefined;
-  const end = _ctx.url.searchParams.get("end") ?? undefined;
+  let { currency } = _ctx.params;
+  let start = _ctx.url.searchParams.get("start") ?? undefined;
+  let end = _ctx.url.searchParams.get("end") ?? undefined;
   try {
-    schema.parse({ currency, start, end });
+    ({ currency, start, end } = schema.parse({ currency, start, end }));
   } catch (e) {
     return new Response(e.message, { status: 400 });
   }
