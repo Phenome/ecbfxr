478fd05de822abc04ea362c102d327952bd93554
Henrique Pinheiro <henrique@gampoc.com>
2024-02-29 14:16:06 -0300

    CORS: allow all origins

--- routes/api/rates/[currency].ts
+++ routes/api/rates/[currency].ts
@@ -9,17 +9,24 @@ const schema = z.object({
   end: z.string().regex(/^\d{4}-\d{2}-\d{2}$/).optional(),
 });
 
-export const handler = async (_req: Request, _ctx: FreshContext) => {
-  let { currency } = _ctx.params;
-  let start = _ctx.url.searchParams.get("start") ?? undefined;
-  let end = _ctx.url.searchParams.get("end") ?? undefined;
+export const handler = async (req: Request, ctx: FreshContext) => {
+  const origin = req.headers.get("Origin") || "*";
+  let { currency } = ctx.params;
+  let start = ctx.url.searchParams.get("start") ?? undefined;
+  let end = ctx.url.searchParams.get("end") ?? undefined;
   try {
     ({ currency, start, end } = schema.parse({ currency, start, end }));
   } catch (e) {
-    return new Response(e.message, { status: 400 });
+    return new Response(e.message, {
+      status: 400,
+      headers: { "Access-Control-Allow-Origin": origin },
+    });
   }
   const data = await getExchangeRate(currency, start, end);
   return new Response(JSON.stringify(data), {
-    headers: { "content-type": "application/json" },
+    headers: {
+      "content-type": "application/json",
+      "Access-Control-Allow-Origin": origin,
+    },
   });
 };
